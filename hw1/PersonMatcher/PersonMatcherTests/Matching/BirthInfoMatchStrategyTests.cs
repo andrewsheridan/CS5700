7d19fb2d4febda20730795c470671fbca5593b99
Andrew Home Computer <sheridan.andrew13@gmail.com>
2016-09-15 11:21:17 -0600

    Added more tests to BirthInfoMatch testss

--- hw1/PersonMatcher/PersonMatcherTests/Matching/BirthInfoMatchStrategyTests.cs
+++ hw1/PersonMatcher/PersonMatcherTests/Matching/BirthInfoMatchStrategyTests.cs
@@ -21,5 +21,35 @@ namespace PersonMatcher.Matching.Tests
             Assert.IsFalse(matcher.Match(c, d));
             Assert.IsFalse(matcher.Match(a, c));
         }
+
+        [TestMethod()]
+        public void MatchTestSuccess()
+        {
+            MatchStrategy matcher = new BirthInfoMatchStrategy();
+
+            Child a = new Child("12345", "F", 1, "Utah", "First", "Middle", "Last", 1, "file", "social", "Name", "Name2", "Name3", 1993, 9, 15, "M");
+            Child b = new Child("12345", "F", 1, "Utah", "First", "Middle", "Last", 2, "file", "social", "Name", "Name2", "Name3", 1993, 9, 15, "M");
+
+            Child c = new Child("54321", "T", 2, "Utah", "First", "Middle", "Last", 3, "file", "social", "Name", "Name2", "Name3", 1993, 9, 15, "M");
+            Child d = new Child("54321", "T", 2, "Utah", "First", "Middle", "Last", 4, "file", "social", "Name", "Name2", "Name3", 1993, 9, 15, "M");
+
+            Assert.IsTrue(matcher.Match(a, b));
+            Assert.IsTrue(matcher.Match(c, d));
+        }
+
+        [TestMethod()]
+        public void MatchTestFailure()
+        {
+            MatchStrategy matcher = new BirthInfoMatchStrategy();
+            //Different newborn screening numbers
+            Child a = new Child("05948", "F", 1, "Utah", "First", "Middle", "Last", 1, "file", "social", "Name", "Name2", "Name3", 1993, 9, 15, "M");
+            Child b = new Child("12345", "F", 1, "Utah", "First", "Middle", "Last", 2, "file", "social", "Name", "Name2", "Name3", 1993, 9, 15, "M");
+            //Different birth orders
+            Child c = new Child("54321", "T", 2, "Utah", "First", "Middle", "Last", 3, "file", "social", "Name", "Name2", "Name3", 1993, 9, 15, "M");
+            Child d = new Child("54321", "T", 1, "Utah", "First", "Middle", "Last", 4, "file", "social", "Name", "Name2", "Name3", 1993, 9, 15, "M");
+
+            Assert.IsFalse(matcher.Match(a, b));
+            Assert.IsFalse(matcher.Match(c, d));
+        }
     }
 }
