234a053e26090ed9ce6fe8d2ce4e1746a3c59191
Andrew Sheridan <A01488483@aggies.usu.edu>
2016-10-27 16:20:29 -0600

    Tried to adapt existing code.

---
+++ hw3/Forests - Version 3 - With Unit Testing/AppLayer/DrawingComponents/ImageFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppLayer.DrawingComponents
+{
+    public class ImageFactory
+    {
+        public string ResourceNamePattern { get; set; }
+        public Type ReferenceType { get; set; }
+
+        private readonly Dictionary<string, ImageWithIntrinsicState> _sharedImages = new Dictionary<string, ImageWithIntrinsicState>();
+
+        public ImageWithAllState GetImage(ImageExtrinsicState extrinsicState)
+        {
+            string resourceName = string.Format(ResourceNamePattern, extrinsicState.ImageType);
+
+            ImageWithIntrinsicState treeWithIntrinsicState;
+            if (_sharedImages.ContainsKey(extrinsicState.ImageType))
+                treeWithIntrinsicState = _sharedImages[extrinsicState.ImageType];
+            else
+            {
+                treeWithIntrinsicState = new ImageWithIntrinsicState();
+                treeWithIntrinsicState.LoadFromResource(resourceName, ReferenceType);
+                _sharedImages.Add(extrinsicState.ImageType, treeWithIntrinsicState);
+            }
+
+            return new ImageWithAllState(treeWithIntrinsicState, extrinsicState);
+        }
+    }
+}
